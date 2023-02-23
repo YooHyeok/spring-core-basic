e4d2a0fd0301f8e195d0f87592d346a8dd924079
YooHyeok <yjk7454@naver.com>
2023-02-23 19:06:42 +0900

    무상태로 설계하여 코드변경 및 테스트

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -13,15 +13,20 @@ class StatefulServiceTest {
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
-        //ThreadA : A사용자 10000원 주문
-        statefulService1.order("userA", 10000);
-        //ThreadB : B사용자 20000원 주문
-        statefulService2.order("userB", 20000);
-        
-        //ThreadA : 사용자 A 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        /* 상태유지(stateful)시 문제 발생 */
+//        statefulService1.order("userA", 10000); // ThreadA : A사용자 10000원 주문
+//        statefulService2.order("userB", 20000); // ThreadB : B사용자 20000원 주문
+//        int price = statefulService1.getPrice(); // ThreadA : 사용자 A 주문 금액 조회
+//        System.out.println("price = " + price);
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+
+        /* 무상태 */
+        int userAPrice = statefulService1.order("userA", 10000);//ThreadA : A사용자 10000원 주문
+        int userBPrice = statefulService2.order("userB", 20000);//ThreadB : B사용자 20000원 주문
+        System.out.println("userBPrice = " + userBPrice);
+        Assertions.assertThat(userAPrice).isEqualTo(10000);
+        Assertions.assertThat(userBPrice).isEqualTo(20000);
+
     }
 
     static class TestConfig {
