c607a837c352fca8601ab340ab7fda38f63f9eb5
YooHyeok <yjk7454@naver.com>
2023-02-23 22:22:03 +0900

    @ComponentScan 어노테이션 주석 정리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(
-        excludeFilters = @ComponentScan.Filter(   type = FilterType.ANNOTATION
-                                                , classes = Configuration.class)
-) // 등록하지 않을것 지정
+@ComponentScan( //default는 @ComponentScan이 붙은 AutoAppConfig 클래스의 패키지가 시작 위치가 된다.
+//        basePackages = "hello.core.member", //member 패키지만 ComponentScan의 대상이 된다. - 모든 라이브러리까지 다 뒤지기때문에 지정해주는게 좋다.
+        basePackageClasses = AutoAppConfig.class, // AutoAppConfig 클래스의 패키지 hello.core 를 탐색시작위치로 지정한다.
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)// 등록하지 않을것 지정
+)
 public class AutoAppConfig {
 
 }
