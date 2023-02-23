e81fb4eaa267dc6a9898926050cd689888f95c2a
YooHyeok <yjk7454@naver.com>
2023-02-23 22:58:57 +0900

    빈 중복등록에 대한 충돌 테스트

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,5 +14,17 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)// 등록하지 않을것 지정
 )
 public class AutoAppConfig {
-
+    /**
+     * 수동으로 등록 빈을 등록한다.
+     * 수동 등록 빈 네임이 중복될경우 수동 빈 등록이 우선권을 가진다.
+     * 수동 빈이 자동 빈을 오버라이딩 해버린다.
+     * 콘솔창에 Overriding bean definition for bean 라고 출력된다.
+     * 스프링 부트에서는 그냥 오류를 출력시켜 버리도록 Overriding이 False로 패치됨. 아래 출력 (properties에 세팅해줘야 overiding이 가능해짐)
+     * Consider renaming one of the beans or enabling overriding by setting spring.main.allow-bean-definition-overriding=true
+     * @return
+     */
+    @Bean(name="memoryMemberRepository") //수동 등록 빈 네임이 중복될경우 수동등록 빈
+    MemberRepository memberRepository () {
+        return new MemoryMemberRepository();
+    }
 }
