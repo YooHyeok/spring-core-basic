07e7d62aabd6ca03ce4fe045c1dc378ee2c7ad71
YooHyeok <yjk7454@naver.com>
2023-03-08 20:16:43 +0900

    InitializingBean 초기화 인터페이스, DisposableBean 소멸 인터페이스 메소드 오버라이딩 적용 - 빈 초기화, 소멸 테스트

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,13 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
     
     public void setUrl(String url) {
@@ -22,5 +23,18 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    @Override // InitializingBean 초기화 인터페이스
+    public void afterPropertiesSet() throws Exception { //의존관계 주입 종료시 호출
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override // DisposableBean 소멸 인터페이스
+    public void destroy() throws Exception { // 빈 종료시 호출
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
