b4f5dbd0dfc46eb1ff3e6d9dbf3c87d84523000e
YooHyeok <yjk7454@naver.com>
2023-03-08 20:08:57 +0900

    싱글톤 주석 정리

--- src/test/java/hello/core/singleton/SingletonService.java
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -1,5 +1,11 @@
 package hello.core.singleton;
 
+/**
+ * 싱글톤 패턴
+ * 클래스의 인스턴스가 딱 1개만 생성되는 것을 보장하는 디자인 패턴
+ * 객체 인스턴스를 2개 이상 생성하지 못하도록 막아야 한다.
+ * private 새성자를 사용해 외부에서 임의로 new 키워드를 사용하지 못하도록 막아야 한다.
+ */
 public class SingletonService {
 
     private static final SingletonService instance = new SingletonService(); // static으로 자기자신 인스턴스 생성
