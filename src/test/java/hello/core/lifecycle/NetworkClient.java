80d27ecfce6d3eb239f77a5475bac520f9df80aa
YooHyeok <yjk7454@naver.com>
2023-03-08 20:43:50 +0900

    @PostConstructor, @PreDestroy를 활용한 Bean생명주기

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 //public class NetworkClient implements InitializingBean, DisposableBean {
 public class NetworkClient {
     private String url;
@@ -23,12 +26,14 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
+    @PostConstruct
     public void init() { //의존관계 주입 종료시 호출
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() { // 빈 종료시 호출
         System.out.println("NetworkClient.close");
         disconnect();
