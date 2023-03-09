5edc9af6c26a3b0110eb8be3fb9e64fd3d091228
YooHyeok <yjk7454@naver.com>
2023-03-09 19:20:05 +0900

    Provider를 통해 오류 해결 - Thread를 통해 1초 간격으로 구분값확인 - 요청이 섞여도 구분 가능, Service에서도 동일한 빈 사용 확인

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -6,22 +6,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.inject.Provider;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
-
+    private final Provider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString(); //url추출
-
+        MyLogger myLogger = myLoggerProvider.get();
         myLogger.setRequestURL(requestURL); //url주입
         myLogger.log("controller test");
+        Thread.sleep(1000); //요청이 섞여도 구분이 가능해진다.
         logDemoService.logic("testId");
         return "ok";
     } 
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -4,11 +4,16 @@ import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Provider;
+
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private final Provider<MyLogger> myLoggerProvider;
+
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.get();
+
         myLogger.log("service ID = " + id);
     }
 }
