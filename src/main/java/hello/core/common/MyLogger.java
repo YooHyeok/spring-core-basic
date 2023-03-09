4032f059ee988257a9cd18fa0e3967a7a5a05a83
YooHyeok <yjk7454@naver.com>
2023-03-09 19:03:45 +0900

    MyLogger request Scope 빈 클래스 구현 , LogDemoController 요청시 URL 주입 , Service 구현 오류발생

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,42 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {//Http 요청당 Bean이 하나씩 생성되고, Http 요청이 끝나는 시점에 소멸된다.
+    private String uuid;
+    private String requestURL;
+
+    /**
+     * 빈이 생성되는 시점에 값을 넣을수 없으므로 setter 생성
+     * @param requestURL
+     */
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    /**
+     * 빈이 생성되는 시점에 초기화 메서드 호출
+     * uuid를 생성하여 저장 - 다른 HTTP 요청과 구분할 구분값
+     */
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+
+    }
+
+    @PreDestroy
+    public void close() { //request가 끝나면 호출된다.
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
